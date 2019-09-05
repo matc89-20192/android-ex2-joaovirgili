a400d50fad9005cb624722890c2ca6942bffbd2a
Joao Virgili <joaovirgili@Joaos-MacBook-Pro.local>
2019-09-05 02:07:13 -0300

    Exercício quase finalizado. Tirar duvida sobre o teste

--- app/src/main/java/matc89/exercicio2/MainActivity.java
+++ app/src/main/java/matc89/exercicio2/MainActivity.java
@@ -1,13 +1,57 @@
 package matc89.exercicio2;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView label;
+    Button btnTrocar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.label = findViewById(R.id.textView);
+        this.btnTrocar = findViewById(R.id.btnTrocar);
+
+        Log.d("create", "onCreate");
+
+        this.btnTrocar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navegaParaOutraActivity();
+            }
+        });
+
+        checaPrefs();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        checaPrefs();
+    }
+
+    void navegaParaOutraActivity() {
+        Intent intent = new Intent(this, OutraActivity.class);
+        startActivity(intent);
+    }
+
+    void checaPrefs() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.contains("nome")) {
+            this.label.setText("Oi, " + prefs.getString("nome", "")+ "!");
+        } else {
+            this.label.setText("Oi!");
+        }
     }
 }
---
+++ app/src/main/java/matc89/exercicio2/OutraActivity.java
@@ -0,0 +1,70 @@
+package matc89.exercicio2;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class OutraActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btnConfirmar;
+    Button btnCancelar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outra);
+
+        this.editText = findViewById(R.id.editText);
+        this.btnConfirmar = findViewById(R.id.btnConfirmar);
+        this.btnCancelar = findViewById(R.id.btnCancelar);
+
+        checaPrefs();
+        this.btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmarClick();
+                finish();
+            }
+        });
+        this.btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limapPrefs();
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    void limapPrefs() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.edit().remove("nome").apply();
+    }
+
+    void checaPrefs() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.contains("nome")) {
+            this.editText.setText(prefs.getString("nome", ""));
+        }
+    }
+
+    void confirmarClick() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String nome = this.editText.getText().toString();
+        if (nome.isEmpty()) {
+            prefs.edit().remove("nome").apply();
+            return;
+        }
+        prefs.edit().putString("nome", nome).apply();
+    }
+}
